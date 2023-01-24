83c95f194178ab85d9331fedb23e38e8cf196222
Joshua_Muniz <joshua.mun97@gmail.com>
2023-01-23 17:46:00 -0700

    Updated files

---
+++ Develop/README.md
@@ -0,0 +1,34 @@
+# Read Me Generator
+
+## Table of Contents
+
+- [Description](#Description)
+
+- [Usage](#Usage)
+
+- [Contributions](#Contributions)
+
+- [Installation](#Installation)
+
+- [Questions](#Questions)
+
+- [License](#License)
+
+## Description
+Short on time? At the end of your project and need to quickly put together a read me? Well, you're in luck because this application does just that! Utilizing node js all you have to do is open up your terminal and open your read me generator, the application does the rest! You will be prompted with questions and all you need to do is answer!
+
+## Usage
+This applications is used to quickly generate a read me file using node JS
+
+## Contributions
+N/A
+
+## Installation
+Utilized Javascript to create functions using the new arrow syntax, as well as utilized const instead of var! first created the variables along with the skeleton functions and built off of that.
+
+## Questions
+Email: joshua.mun97@gmail.com
+Github: joshuamun97
+
+## License
+N/A
--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,99 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questionsInput = [
+    {type: 'input',
+    name: 'projectTitle',
+    message: 'What is your Project Title?'
+    },
 
+    {type: 'input',
+    name: 'Description',
+    message: 'What is the description of your project?'
+    },
+
+    {type: 'input',
+    name: 'Usage',
+    message: 'What would you use this Application for?'
+    },
+
+    {type: 'input',
+    name: 'Contributions',
+    message: 'Any Notable Contributers?'
+    },
+
+    {type: 'input',
+    name: 'Installation',
+    message: 'Please input the steps you took to install/create this application'
+    },
+
+    {type: 'input',
+    name: 'email',
+    message: 'Please input your email'
+    },
+
+    {type: 'input',
+    name: 'github',
+    message: 'Please input your github username'
+    },
+    
+    {type: 'input',
+    name: 'License',
+    message: 'Lastly, Do you have a licence you wish to include? if none, please input N/A'
+    },
+];
+
+inquirer.prompt(questionsInput)
+    .then(answers => {
+        createMD(answers)
+
+});
 // TODO: Create a function to write README file
+const createMD = data => {
+    
+const MD = 
+`# ${data.projectTitle}
+
+## Table of Contents
+
+- [Description](#Description)
+
+- [Usage](#Usage)
+
+- [Contributions](#Contributions)
+
+- [Installation](#Installation)
+
+- [Questions](#Questions)
+
+- [License](#License)
+
+## Description
+${data.Description}
+
+## Usage
+${data.Usage}
+
+## Contributions
+${data.Contributions}
+
+## Installation
+${data.Installation}
+
+## Questions
+Email: ${data.email}
+Github: ${data.github}
+
+## License
+${data.License}
+`
+    console.log(MD);
+    fs.writeFile('README.md', MD, err => {
+        err ? console.log(err) : console.log('Success!')
+    });
+};
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
